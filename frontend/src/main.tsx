b17d9ee863cf634f9f9e90cfaa1910d47140f4b5
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2026-05-05 23:15:57 +0545

    (Authentication): FE - profile form and controller, common input field, react hot toast

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,29 @@
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+import { Toaster } from "react-hot-toast";
 
 const router = createRouter({
   routeTree,
-  defaultPreload: 'intent',
+  defaultPreload: "intent",
   scrollRestoration: true,
-})
+});
 
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
-const rootElement = document.getElementById('app')!
+const rootElement = document.getElementById("app")!;
 
 if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
-  root.render(<RouterProvider router={router} />)
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+      <Toaster />
+    </StrictMode>,
+  );
 }
--- frontend/src/routes/learner/_layout/profile.tsx
+++ frontend/src/routes/learner/_layout/profile.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
+import ProfileForm from "@/components/admin/profileForm";
 
 export const Route = createFileRoute("/learner/_layout/profile")({
   component: RouteComponent,
@@ -9,8 +10,7 @@ function RouteComponent() {
     <div className="w-full xl:w-1/2 mx-auto">
       <div className="bg-white/90 dark:bg-gray-700/30 p-6 rounded-xl border border-gray-200 dark:border-gray-700">
         <h1 className="text-2xl font-semibold">Profile</h1>
-
-        <h1>Form</h1>
+        <ProfileForm />
       </div>
     </div>
   );
